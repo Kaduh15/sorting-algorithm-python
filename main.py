17418f11ff42ce9e9f5fac67d70ba82feb4088e2
Kadu <kadu.silva2014@gmail.com>
2023-03-22 17:33:22 -0300

    ♻ refactor: removes the function menor_maior

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from timeit import timeit
 
 
@@ -15,13 +16,14 @@ def sorte(list: list[int]) -> list[int]:
         return
 
     result = []
+    min_value = min(list)
 
-    min, max = menor_maior(list)
-    result.append(min)
-    result.append(max)
-    list.remove(min)
-    if max in list:
-        list.remove(max)
+    result.append(min_value)
+    list.remove(min_value)
+    if len(list) > 0:
+        max_value = max(list)
+        result.append(max_value)
+        list.remove(max_value)
 
     sorted_list = sorte(list)
 
@@ -34,25 +36,6 @@ def sorte(list: list[int]) -> list[int]:
     return sorted_list
 
 
-def menor_maior(list: list[int]):
-    """
-    It returns the minimum and maximum values in a list
-
-    :param list: list[int]: This is the list of integers
-                            that we're going to be searching through
-    :type list: list[int]
-    :return: A tuple with the minimum and maximum values of the list.
-    """
-    min = list[0]
-    max = min
-    for value in list:
-        if value < min:
-            min = value
-        if value > max:
-            max = value
-    return (min, max)
-
-
 def test_time(x: int = 1):
     """
     It prints a message, creates a list of numbers, and then times how long it
@@ -63,22 +46,29 @@ def test_time(x: int = 1):
     :type x: int (optional)
     """
     print(('=' * 10) + f" \033[32m Testing time {x}X\033[0m  " + ('=' * 10))
-    numbers = [1, 2, 3, 4, 5]
-    numbers = numbers * x
-    total_items = len(numbers)
-    print(
-        f'''Foi ordenado uma list de {total_items} itens\b
-Em \033[32m{timeit(lambda: sorted(numbers))}\033[0m segundos
-\033[33m* com a função nativa do Python -> sorted()\033[0m''')
+    numbers = range(1, 10_001)
+    total_numbers = len(numbers)
+    len_max_numbers = total_numbers if x * 5 > total_numbers else x * 5
 
-    print(
-        f'''
-Foi ordenado uma list de {total_items} itens
-Em \033[32m{timeit(lambda: sorte(numbers))}\033[0m segundos
-\033[33m* usando a minha função -> sorte()
-\033[0m''')
+    numbers = numbers = random.sample(range(10, 10_000), len_max_numbers)
+    total_items = len(numbers)
+
+    numbers_1 = numbers.copy()
+    numbers_2 = numbers.copy()
+
+    print((
+        f'Foi ordenado uma list de {total_items} itens\n'
+        f'Em \033[32m{timeit(lambda: sorte(numbers_1))}\033[0m segundos\n'
+        f'\033[33m* usando a minha função -> sorte()\033[0m\n'
+    ))
+
+    print((
+        f'Foi ordenado uma list de {total_items} itens\n'
+        f'Em \033[32m{timeit(lambda: sorted(numbers_2))}\033[0m segundos\n'
+        f'\033[33m* com a função nativa do Python -> sorted()\033[0m\n'
+    ))
 
 
 if __name__ == '__main__':
     test_time(1)
-    test_time(100)
+    # test_time(100)
