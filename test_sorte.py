38e434b7997e06e8b7010a8a1e56b1a1fbfff7fc
Kadu <kadu.silva2014@gmail.com>
2023-03-22 19:25:31 -0300

    ✅ test: created test for 'sorte'

---
+++ test_sorte.py
@@ -0,0 +1,8 @@
+from main import sorte
+
+
+def test_sorte():
+    assert list(sorte([2, 3, 1])) == [1, 2, 3]
+    assert list(sorte([1, 6, 7, 1, 2])) == [1, 1, 2, 6, 7]
+    assert list(sorte([4, 3, 4, 3, 1, 3, 2])) == [1, 2, 3, 3, 3, 4, 4]
+    assert list(sorte([1, 1, 1, -4, 4, 1, 1])) == [-4, 1, 1, 1, 1, 1, 4]
