408be1087f7396de35bb591b058a8460ed91f2ca
Kadu <kadu.silva2014@gmail.com>
2023-03-25 15:40:51 -0300

    ♻refactor: Function "sorte" without recursion

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
 from collections import deque
 
+from stack import Stack
+
 
 def sorte(list: list[int]) -> list[int]:
     """
     It sorts a list of integers by recursively finding the
     minimum and maximum values of the list,
     appending them to a new list, and then removing them from the original list
-
     :param list: list[int] -> The list of integers to be sorted
     :type list: list[int]
     :return: A list with the smallest and largest values of the list.
     """
-    if len(list) == 0:
-        return
-    if len(list) == 1:
-        deque().append(list[0])
-
+    list = list.copy()
+    stack = Stack()
     result = deque()
-    min_value = min(list)
 
-    result.append(min_value)
-    list.remove(min_value)
-    if len(list) > 0:
-        max_value = max(list)
-        result.append(max_value)
-        list.remove(max_value)
+    while len(list) > 0:
+        menor_maior = []
+        min_value = min(list)
+        menor_maior.append(min_value)
+        list.remove(min_value)
+        if len(list) > 0:
+            max_value = max(list)
+            menor_maior.append(max_value)
+            list.remove(max_value)
+        stack.add(menor_maior)
 
-    sorted_list = sorte(list)
+    while len(stack) > 0:
+        current_stack = stack.pop()
+        if len(current_stack) == 1:
+            result.append(current_stack[0])
+        else:
+            result.appendleft(current_stack[0])
+            result.append(current_stack[1])
 
-    if not sorted_list:
-        return result
-
-    sorted_list.append(result.pop())
-    sorted_list.appendleft(result.pop())
-
-    return sorted_list
+    return result
