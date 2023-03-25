e44f24fe0ada0d6e816f46bb9be7b1af3d6bc2e4
Kadu <kadu.silva2014@gmail.com>
2023-03-25 15:36:53 -0300

    🧪 test: Organize tests in a folder

--- __tests__/test_sorte.py
+++ __tests__/test_sorte.py
@@ -3,6 +3,7 @@ from main import sorte
 
 def test_sorte():
     assert list(sorte([2, 3, 1])) == [1, 2, 3]
+    assert list(sorte([2, 9, 3, 1])) == [1, 2, 3, 9]
     assert list(sorte([1, 6, 7, 1, 2])) == [1, 1, 2, 6, 7]
     assert list(sorte([4, 3, 4, 3, 1, 3, 2])) == [1, 2, 3, 3, 3, 4, 4]
-    assert list(sorte([1, 1, 1, -4, 4, 1, 1])) == [-4, 1, 1, 1, 1, 1, 4]
+    assert list(sorte([3, 1, 2, -4, 4, 1])) == [-4, 1, 1, 2, 3, 4]
